472893ca1ab8d4821926692071a9dc712e0c0129
OscarMaestre <profesor.oscar.gomez@gmail.com>
2014-09-24 12:03:27 +0200

    Actualizado PDF y HTML

--- reconstruir.bat
+++ reconstruir.bat
@@ -1,8 +1,8 @@
 call make singlehtml
 call make latex
 cd _build\latex
-D:\miktex\miktex\bin\pdflatex ServiciosProcesos.tex
-D:\miktex\miktex\bin\pdflatex ServiciosProcesos.tex
-start ServiciosProcesos.pdf
+pdflatex ServiciosProcesos.tex
+pdflatex ServiciosProcesos.tex
 cd ..
 cd ..
+
