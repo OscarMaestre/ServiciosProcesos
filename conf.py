3a92516a0f0b436b7fbeffd290878242c1ceeaac
oscar <oscar@example.com>
2016-09-27 07:46:15 +0200

    Modificado curso

--- conf.py
+++ conf.py
@@ -64,7 +64,7 @@ language = "es"
 # non-false value, then it is used:
 #today = ''
 # Else, today_fmt is used as the format for a strftime call.
-today_fmt = '2014-2015'
+today_fmt = 'Curso 2016/2017'
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
