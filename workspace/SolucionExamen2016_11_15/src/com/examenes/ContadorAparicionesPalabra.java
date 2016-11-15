190ea565ae37fd296d455e3969cd616ec5013fbf
Oscar Gomez <ogomez@informatica.local>
2016-11-15 14:49:01 +0100

    Solucion

---
+++ workspace/SolucionExamen2016_11_15/src/com/examenes/ContadorAparicionesPalabra.java
@@ -0,0 +1,70 @@
+package com.examenes;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.ArrayList;
+
+public class ContadorAparicionesPalabra {
+	private static String EXTENSION_RESULTADOS=".res";
+	
+	/* Dada una subcadena, nos dice cuantas veces aparece la subcadena
+	 * en dicha cadena. Usaremos el metodo String.indexOf para
+	 * ir buscando todas las apariciones de la subcadena
+	 */
+	private static long contarApariciones(String cadena, String subcadena){
+		int posBusqueda=0;
+		int posAparicion;
+		long apariciones=0;
+		while (posBusqueda <= cadena.length() ){
+			/* Buscamos la primera aparicion de la subcadena*/
+			posAparicion=cadena.indexOf(subcadena, posBusqueda);
+			/* Si aparece, incrementamos el contador...*/
+			if (posAparicion!=-1){
+				apariciones++;
+				/* Y la siguiente busqueda se hará a partir de la 
+				 * siguiente posición correcta */
+				posBusqueda = posBusqueda + subcadena.length() + 1;
+			}
+		}
+		return apariciones;
+	}
+	/* Dados una palabra (argumento 0) 
+	 * y un fichero (argumento 1) se 
+	 * contabiliza el total de apariciones
+	 * de esa palabra en el fichero y se almacena
+	 * en un fichero con extensión ".res". Observar
+	 * que la extensión se añade al fichero, el cual
+	 * puede acabar teniendo 2 extensiones (como
+	 * por ejemplo "fichero1.txt.res" */
+	public static void main(String[] args) throws IOException {
+		long aparicionesDeLaPalabra	=0;
+		String palabraParaBuscar	=args[0];
+		String nombreFichero		=args[1];
+		//Recuperamos las líneas del fichero
+		ArrayList<String> lineas;
+		lineas=UtilidadesFicheros.getLineasFichero(nombreFichero);
+		//Vamos recorriendo las líneas...
+		for (String linea : lineas) {
+			linea.
+			//Y si encontramos la palabra...
+			if ( linea.contains( palabraParaBuscar ) ) {
+				//..incrementamos el contador
+				aparicionesDeLaPalabra++;
+			}
+			//Fin del for que recorre las lineas
+		}
+		//Ya tenemos la cantidad de apariciones
+		//así que almacenamos los resultados
+		PrintWriter pw;
+		pw=UtilidadesFicheros.getPrintWriter(
+				nombreFichero + EXTENSION_RESULTADOS);
+		
+		pw.println(nombreFichero+":"+
+					palabraParaBuscar+":"+
+					aparicionesDeLaPalabra);
+		pw.close();
+		
+
+	}
+
+}
---
+++ workspace/SolucionExamen2016_11_15/src/com/examenes/LanzadorContadoresApariciones.java
@@ -0,0 +1,36 @@
+package com.examenes;
+
+import java.io.File;
+import java.io.IOException;
+
+public class LanzadorContadoresApariciones {
+	private static String EXTENSION_ERRORES=".err";
+	public static void main(String[] args) throws IOException{
+		String palabraParaBuscar=args[0];
+		
+		//Se lanzan los procesos...
+		ProcessBuilder[] constructores;
+		constructores=new ProcessBuilder[args.length-1];
+		//..pero saltando la posición 0 (la palabra)
+		for (int i=1; i<args.length; i++){
+			
+			
+			String ficheroDondeBuscar;
+			ficheroDondeBuscar=args[i];
+			
+			String ficheroDeErrores;
+			ficheroDeErrores=ficheroDondeBuscar + EXTENSION_ERRORES;
+			
+			//Se prepara lo necesario para lanzar el proceso...
+			constructores[i-1]=new ProcessBuilder();
+			constructores[i-1].command(
+					"java","com.examenes.ContadorAparicionesPalabra",
+					palabraParaBuscar, ficheroDondeBuscar);
+			constructores[i-1].redirectError(
+					new File(ficheroDeErrores) );
+			//..y se ejecuta
+			constructores[i-1].start();
+		}
+		
+	}
+}
