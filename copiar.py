4d72d2333cf2213d9ba5426207a860b508936d8f
Oscar G.G <profesor.oscar.gomez@gmail.com>
2016-11-24 14:11:51 +0100

    Incluida documentacion

--- copiar.py
+++ copiar.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 import os, shutil
 
-RUTA_DIRECTORIO_DESTINO = ".." + os.sep + "oscarmaestre.github.io" + os.sep + "servicios"
+RUTA_DIRECTORIO_DESTINO ="docs"
 RUTA_APUNTES = "_build" + os.sep + "singlehtml"
 print ("Borrando " + RUTA_DIRECTORIO_DESTINO)
 shutil.rmtree ( RUTA_DIRECTORIO_DESTINO , ignore_errors=True)
